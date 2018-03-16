5ee8cbdb4a4c7d0b779af39fb68f7a78a04a1d41
Francesco Franchina <cescus92@gmail.com>
2018-03-16 01:08:06 +0100

    Plugin file created

---
+++ fablab-management.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: FabLab Management
+Description: Manage all the aspect of your FabLab: events, projects and users. Requires Advanced Custom Field plugin.
+Version: 0.9
+Author: Francesco Franchina
+*/
+
