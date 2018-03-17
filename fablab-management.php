b8a45cca4e4295687b3957f1d04f2ffe89e6816a
Francesco Franchina <cescus92@gmail.com>
2018-03-17 23:57:31 +0100

    Small refactory (now classes are used)

--- fablab-management.php
+++ fablab-management.php
@@ -10,3 +10,5 @@ Author: Francesco Franchina
 define('ACF_EARLY_ACCESS', '5');
 
 require_once plugin_dir_path(__FILE__) . '/lib/events.php';
+
+events::register_post_type();
--- lib/events.php
+++ lib/events.php
@@ -1,42 +1,47 @@
 <?php
 
-// CUSTOM POST
-
-add_action( 'init', 'cptui_register_my_cpts_evento' );
 function cptui_register_my_cpts_evento() {
-	$labels = array(
-		"name" => 'Eventi',
-		"singular_name" => 'Evento',
-		"menu_name" => 'Eventi',
-		"all_items" => 'Tutti gli Eventi',
-		"add_new" => 'Aggiungi nuovo',
-		"add_new_item" => 'Aggiungi nuovo Evento',
-		"edit_item" => 'Modifica Evento',
-		"new_item" => 'Nuovo Evento',
-		"view_item" => 'Visualizza Evento',
-		"search_items" => 'Cerca Evento',
-		"not_found" => 'Nessun Evento trovato',
-		);
+    $labels = array(
+        "name" => 'Eventi',
+        "singular_name" => 'Evento',
+        "menu_name" => 'Eventi',
+        "all_items" => 'Tutti gli Eventi',
+        "add_new" => 'Aggiungi nuovo',
+        "add_new_item" => 'Aggiungi nuovo Evento',
+        "edit_item" => 'Modifica Evento',
+        "new_item" => 'Nuovo Evento',
+        "view_item" => 'Visualizza Evento',
+        "search_items" => 'Cerca Evento',
+        "not_found" => 'Nessun Evento trovato',
+        );
 
-	$args = array(
-		"label" => 'Eventi',
-		"labels" => $labels,
-		"description" => "",
-		"public" => true,
-		"show_ui" => true,
-		"show_in_rest" => false,
-		"rest_base" => "",
-		"has_archive" => true,
-		"show_in_menu" => true,
-		"exclude_from_search" => false,
-		"capability_type" => "post",
-		"map_meta_cap" => true,
-		"hierarchical" => false,
-		"rewrite" => array( "slug" => "evento", "with_front" => true ),
-		"query_var" => true,
-		"menu_position" => 6,"menu_icon" => "dashicons-calendar-alt",		
-		"supports" => array( "title", "editor", "thumbnail" ),		
-		"taxonomies" => array( "category" ),		
-	);
-	register_post_type( "evento", $args );
+    $args = array(
+        "label" => 'Eventi',
+        "labels" => $labels,
+        "description" => "",
+        "public" => true,
+        "show_ui" => true,
+        "show_in_rest" => false,
+        "rest_base" => "",
+        "has_archive" => true,
+        "show_in_menu" => true,
+        "exclude_from_search" => false,
+        "capability_type" => "post",
+        "map_meta_cap" => true,
+        "hierarchical" => false,
+        "rewrite" => array( "slug" => "evento", "with_front" => true ),
+        "query_var" => true,
+        "menu_position" => 6,"menu_icon" => "dashicons-calendar-alt",
+        "supports" => array( "title", "editor", "thumbnail" ),
+        "taxonomies" => array( "category" ),
+    );
+    register_post_type( "evento", $args );
+}
+
+
+class events {
+
+    static function register_post_type() {
+        add_action( 'init', 'cptui_register_my_cpts_evento' );
+    }
 }
